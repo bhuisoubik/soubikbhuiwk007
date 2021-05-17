01a95900afd7ee7f4c32d2e27c9a65bc880c4171
Soubik Bhui <soubik.bhui16@gmail.com>
2021-05-17 11:30:32 +0530

    Update sample.py

--- dist/sample.py
+++ dist/sample.py
@@ -2,6 +2,9 @@ import js as n
 import js
 from js import nothing
 from jsn import nothing as no
+from jsn import func, var
+from jsn import func as fn, var as vr
+
 
 print("Hello World from python of the web")
 name = input('Enter your name')
